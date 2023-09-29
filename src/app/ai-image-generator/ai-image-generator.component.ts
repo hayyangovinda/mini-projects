dd175b228cc6a0b189e6f61c7a03ae3575c369df
Hayyan <hayyan.gov@gmail.com>
2023-09-29 09:43:41 +0400

    ai generator-images service

--- src/app/ai-image-generator/ai-image-generator.component.ts
+++ src/app/ai-image-generator/ai-image-generator.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { AiApiService } from './ai-api.service';
 
 @Component({
   selector: 'app-ai-image-generator',
   templateUrl: './ai-image-generator.component.html',
-  styleUrls: ['./ai-image-generator.component.css']
+  styleUrls: ['./ai-image-generator.component.css'],
 })
 export class AiImageGeneratorComponent {
+  constructor(private aiApiService: AiApiService) {}
 
+  userInput: string = '';
+  quantity = 1;
+  apiResponse!: any;
+  generateImages() {
+    this.aiApiService
+      .generateAiImages(this.userInput, this.quantity)
+      .subscribe((resp) => {
+        this.apiResponse = resp;
+
+        console.log(this.apiResponse);
+      });
+  }
 }
