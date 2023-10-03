2f98b17e229942757302281bcf3f2ab803491062
Hayyan <hayyan.gov@gmail.com>
2023-10-03 11:35:41 +0400

    change detection strategy to onpush on dad jokes

--- src/app/dad-jokes-generator/dad-jokes-generator.component.ts
+++ src/app/dad-jokes-generator/dad-jokes-generator.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ComponentRef,
+} from '@angular/core';
 import { DadJokesService } from './dad-jokes.service';
 
 @Component({
   selector: 'dad-jokes-generator',
   templateUrl: './dad-jokes-generator.component.html',
   styleUrls: ['./dad-jokes-generator.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DadJokesGeneratorComponent {
-  constructor(private dadJokesService: DadJokesService) {}
+  constructor(
+    private dadJokesService: DadJokesService,
+    private ref: ChangeDetectorRef
+  ) {}
   jokeText: string = '';
   buttonText: string = 'Tell me a joke';
   jokeResponse!: any;
@@ -23,6 +32,7 @@ export class DadJokesGeneratorComponent {
         this.jokeText = this.jokeResponse[0].joke;
         this.disableButton = false;
         this.buttonText = 'Tell me a joke';
+        this.ref.detectChanges();
       },
       () => {
         this.jokeText = 'An error happened.Please try again';
